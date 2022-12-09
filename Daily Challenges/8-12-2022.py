dd11a3ee3d72965d30ac955188f6d8f4a91193d7
Naman Sharma <namansharma18899@gmail.com>
2022-12-09 13:31:39 +0530

    removed pychache

--- Daily Challenges/8-12-2022.py
+++ Daily Challenges/8-12-2022.py
@@ -14,11 +14,6 @@ class Solution:
     def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
         arr1 = self.get_leaf_nodes(root1)
         arr2= self.get_leaf_nodes(root2)
-        # for each in arr1:
-            # if each is not None:
-            #     print('val -> ', each.val)
-            # else:
-            #     print('nonee')
         arr1 = [int(element.val) for element in arr1]
         arr2 = [int(element.val) for element in arr2]
         return True if arr1==arr2 else False
@@ -27,24 +22,11 @@ class Solution:
         if root==None:
             return []
         if self.leaf(root):
-            return [root.val]
+            return [root]
         result = []
         result+=self.get_leaf_nodes(root.left)
         result+=self.get_leaf_nodes(root.right)
         return result
-        # if self.leaf(root1) or self.leaf(root2):
-        #     print('leaf -> ', root1.val, root2.val)
-        #     try:
-        #         if int(root1.val) == int(root2.val):
-        #             print(root1.val)
-        #             return True
-        #         return False
-        #     except Exception as e:
-        #         return False
-        # else:
-        #     return True if self.leafSimilar(root1.left, root2.left) \
-        #         or self.leafSimilar(root1.right, root2.right) else False
-        # return True if result_left_subtree or result_right_subtree else False
 
 
 def fun(arr):
