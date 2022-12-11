e2eabf6815529782633e8985a383b4cdce3f03a9
Naman Sharma <namansharma18899@gmail.com>
2022-12-11 20:08:16 +0530

    daily challenge

--- Leetcode/utils/utility.py
+++ Leetcode/utils/utility.py
@@ -11,11 +11,48 @@ class TreeNode:
 
 def insertLevelOrder(arr, i, n):
     root = None
-    # Base case for recursion 
     if i < n:
-        root = TreeNode(arr[i])
-        # insert left child 
+        if not arr[i].isnumeric():
+            return None
+        root = TreeNode(int(arr[i]))
         root.left = insertLevelOrder(arr, 2 * i + 1, n)
-        # insert right child 
-        root.right = insertLevelOrder(arr, 2 * i + 2, n)  
+        root.right = insertLevelOrder(arr, 2 * i + 2, n)        
     return root
+
+def printLevelOrder(root):
+    h = height(root)
+    for i in range(1, h+1):
+        printCurrentLevel(root, i)
+ 
+ 
+# Print nodes at a current level
+def printCurrentLevel(root, level):
+    if root is None:
+        return
+    if level == 1:
+        print('Value -> ', root.val, end="\n")
+    elif level > 1:
+        printCurrentLevel(root.left, level-1)
+        printCurrentLevel(root.right, level-1)
+
+def height(node):
+    if node is None:
+        return 0
+    else:
+        # Compute the height of each subtree
+        lheight = height(node.left)
+        rheight = height(node.right)
+ 
+        # Use the larger one
+        if lheight > rheight:
+            return lheight+1
+        else:
+            return rheight+1
+
+def is_leaf(root: TreeNode):
+        if root==None or root.val=='None':
+            return False  
+        if (root.left==None and root.right==None):
+            return True
+        else:
+            return False
