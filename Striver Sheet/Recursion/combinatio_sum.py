efa34bcbcdde84352c6340be9319ff290df7b72b
Naman Sharma <namansharma18899@gmail.com>
2023-07-27 17:40:50 +0530

    :sparkles: ss qust

---
+++ Striver Sheet/Recursion/combinatio_sum.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+from typing import List
+
+# from utils import *
+"""
+INTUTION -> 
+premise: number of unique sets of {[a:b], [c:d]}, no two items with same keys have same values
+Scenario:
+
+if (len(keys)== same){
+    if (set(keys)== same)){
+        compare_two_dicts( )
+    } 
+}
+ISSUE -> HOW TO COMPARE MULTIPLE DICTS ???
+"""
+
+"""
+Personal Try
+"""
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+#         res = self.find_com([] + sorted(candidates), target, 0, 0)
+
+#     def find_com(self, nums, target, index, curr_sum, curr_dict=defaultdict(int)):
+#         # +ve passed
+#         # boundary
+#         if index >= len(nums):
+#             return curr_dict if curr_sum == target else [{}]
+#         if nums[index] > target:
+#             return [{}]
+#         res = []
+#         # Skip this
+#         res1 = self.find_com(nums, target, index + 1, curr_sum, curr_dict)
+#         curr_dict[nums[index]] += 1
+#         curr_sum += nums[index]
+#         # incld this and move on
+#         res2 = self.find_com(nums, target, index + 1, curr_sum, curr_dict)
+#         # incld this again
+#         res3 = self.find_com(nums, target, index, curr_sum, curr_dict)
+#         """Now filter the duplicate ones out"""
+
+
+"""
+Solution:
+"""
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        pass
+
+def fun(args):
+    sol = Solution()
+    return sol.fun(args)
+
+
+if __name__ == "__main__":
+    print(fun(input().split(",")))
