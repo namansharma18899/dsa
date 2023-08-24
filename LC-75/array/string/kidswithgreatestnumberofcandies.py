ad0477a5cd8c783d247a8f1cf3ab3e7ece72e617
Naman Sharma <namansharma18899@gmail.com>
2023-08-24 11:29:29 +0530

    quest

--- LC-75/array/string/kidswithgreatestnumberofcandies.py
+++ LC-75/array/string/kidswithgreatestnumberofcandies.py
@@ -1,3 +1,12 @@
 class Solution:
     def kidsWithCandies(self, candies, extraCandies: int):
-        pass
+        greatest = max(candies)
+        return [True if candy+extraCandies>=greatest else False for candy in candies ]
+    
+
+
+sol = Solution()
+candies = [4,2,1,1,2]
+extra = 1
+
+print(sol.kidsWithCandies(candies, extra))
