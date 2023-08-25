75319317f94c0d784c6218fbe676d1b8240fdbbf
Naman Sharma <namansharma18899@gmail.com>
2023-08-25 23:00:23 +0530

    cannt comporehend the solution

--- LC-75/array/string/Increasing_triplet_sequence.py
+++ LC-75/array/string/Increasing_triplet_sequence.py
@@ -1,21 +1,33 @@
+from Leetcode.utils.utility import timer
+
+# class Solution:
+#     @timer
+#     def increasingTriplet(self, num):
+#         """
+#         brute force: 
+#         O(N3)
+        # """
+        # for index in range(0, len(num)):
+        #     for j in range(index+1, len(num)):
+        #         if num[j] > num[index]:
+        #             for k in range(j+1, len(num)):
+        #                 if num[k] > num[j]:
+        #                     return True
+        # return False
 class Solution:
-    def increasingTriplet(self, num):
-        """
-        brute force: 
-        O(N3)
-        """
-        for index in range(0, len(num)):
-            for j in range(index+1, len(num)):
-                if num[j] > num[index]:
-                    for k in range(j+1, len(num)):
-                        if num[k] > num[j]:
-                            return True
+    def increasingTriplet(self, nums) -> bool:
+        first = second = float('inf') 
+        for n in nums: 
+            if n <= first: 
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
         return False
 
 
 
-
 sol = Solution()
-arr = []
-
+arr = [9,8,7,6,5]
 print(sol.increasingTriplet(arr))
