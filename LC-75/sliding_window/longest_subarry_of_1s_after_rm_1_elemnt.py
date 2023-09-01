06c185e3f779c761828d682b4163c9bae61cfd4f
Naman Sharma <namansharma18899@gmail.com>
2023-09-01 17:32:32 +0530

    learnt concept

--- LC-75/sliding_window/longest_subarry_of_1s_after_rm_1_elemnt.py
+++ LC-75/sliding_window/longest_subarry_of_1s_after_rm_1_elemnt.py
@@ -1,24 +1,19 @@
-from typing import List
-
-
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         """
         left highest and right highes
         """
-        i = 0
-        prevZ = None
-        psum =0
         res = 0
-        while(i < len(nums)):            
-            if nums[i]==0:
-                if not prevZ:
-                    prevZ = i
-                else:
-                    psum = i - prevZ -1
-                    prevZ = i
-            else:
-                psum+=1
-            res = max(res, psum)
-            i+=1
-        return res if prevZ is not None else res-1
+        index = 0
+        start= 0
+        zcount = 0
+        while(index < len(nums)):
+            if nums[index]==0:
+                zcount+=1
+            while(zcount> 1):
+                if nums[start]==0:
+                    zcount-=1
+                start+=1
+            res= max(res, index-start)
+            index+=1
+        return res 
