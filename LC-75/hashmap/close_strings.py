38d224ede80c88d2d5eda94f318e69e1e643930a
Naman Sharma <namansharma18899@gmail.com>
2023-09-02 19:12:54 +0530

    anothr one

--- LC-75/hashmap/close_strings.py
+++ LC-75/hashmap/close_strings.py
@@ -1,8 +1,9 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
+        """
         f1 = defaultdict(int)
         f2 = defaultdict(int)
         if len(word1)!=len(word2):
@@ -24,3 +25,17 @@ class Solution:
         if v1!=v2:
             return False
         return True
+        """
+        c1 = Counter(word1)                                         #          c1 = {'a':2, 'b':3, 'c':1}
+        c2 = Counter(word2)                                         #          c1 = {'a':2, 'b':3, 'c':1}
+
+        count1 = sorted(c1.values())                                #      count1 = [1, 2, 3]
+        count2 = sorted(c2.values())                                #      count2 = [1, 2, 3]
+
+        set1 = set(word1)                                           #        set1 = {'c', 'b', 'a'}
+        set2 = set(word2)                                           #        set2 = {'a', 'c', 'b'}
+
+        if count1 == count2 and set1 == set2:                       #      return True
+            return True
+
+        return False
