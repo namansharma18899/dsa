746e1d0e729827867235cfaffcf472c4ed40e1f8
Naman Sharma <namansharma18899@gmail.com>
2023-09-03 20:28:16 +0530

    Ihave redeemed myself

--- LC-75/hashmap/equal_row_col_pais.py
+++ LC-75/hashmap/equal_row_col_pais.py
@@ -1,27 +1,9 @@
-from collections import Counter, defaultdict
-from typing import List
-
-
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        counter = [0]*(len(grid)*len(grid[0]))#mxn
-        freq = []
-        res  = 0
-        for row in grid:
-            freq.append(row)
-        freq2 = []
-        for i in range(len(grid[0])):
-            col = []
-            for j in range(len(grid)):
-                col.append(grid[j][i])
-            freq2.append(col)
-        for each in freq:
-            for j in freq2:
-                if each == j:
-                    res+=1
+        rows_counts = Counter(map(tuple, grid))
+        columns = zip(*grid)
+        res = 0
+        for each in columns:
+            if each in rows_counts:
+                res+=rows_counts[each]
         return res
-    
-
-obj = Solution()
-grid = [[3,2,1],[1,7,6],[2,7,7]]
-print(obj.equalPairs(grid))
