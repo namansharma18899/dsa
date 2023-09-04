539be57aaf26707993a77262844093a791d9b5db
Naman Sharma <namansharma18899@gmail.com>
2023-09-04 19:07:23 +0530

    404 brain not found

--- Striver Sheet/Array2/Count Inversions.py
+++ Striver Sheet/Array2/Count Inversions.py
@@ -21,5 +21,26 @@ def takeInput() :
     return arr, n
 
 # Main.
-arr, n = takeInput()
-print(getInversions(arr, n))
+# arr, n = takeInput()
+# print(getInversions(arr, n))
+
+
+"""
+2nd try 
+"""
+
+def c_inversion(arr):
+    i = 0
+    j = len(arr)-1
+    count=0
+    while(i < j):
+        if arr[i] > arr[j]:
+            temp = j+0
+            while(i < temp and arr[i] >  arr[temp]):
+                count+=1
+                temp-=1
+        i+=1
+    return count
+
+arr = [2,5,1,3,4]
+print(c_inversion(arr))
