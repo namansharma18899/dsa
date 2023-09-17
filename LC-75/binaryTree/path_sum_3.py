4d3a7f02590afde9c69012e4fc47949f7bdc6245
Naman Sharma <namansharma18899@gmail.com>
2023-09-17 09:40:00 +0530

    Learnt solution dfs traversal

--- LC-75/binaryTree/path_sum_3.py
+++ LC-75/binaryTree/path_sum_3.py
@@ -1,46 +1,39 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
-from typing import Optional
-
-
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        if not root:
-            return 0
-        count = 0
-        if root.val==targetSum:
-            count+=1
-        if root.right and root.right.val == targetSum:
-            count+=1
-        if root.left and root.left.val == targetSum:
-            count+=1
-        res = self.psum(root.left, targetSum, root.val) + self.psum(root.right, targetSum, root.val)
-        return count + res
+        self.result = 0
+        self.dfs(root, targetSum)
+        return self.result
 
-    def psum(self, root, target, psum):
-        if not root:
-            return 0
-        count = 0
-        if root.val + psum==target:
-            print('root + target', root.val, psum)
-            count+=1
-        # skip
-        if root.left and root.left.val == target:
-            count+=1
-        r1 = self.psum(root.left, target, root.val)
-        r3 = self.psum(root.left, target, psum + root.val)
-        # use
-        if root.right and root.right.val == target:
-            count+=1
-        r2 = self.psum(root.right, target, root.val)
-        r4 = self.psum(root.right, target, psum + root.val)
+    def dfs(self, node, targetSum):
+        if not node:
+            return
+        #Preorder
+        self.test(node, targetSum)
+        self.dfs(node.left, targetSum)
+        self.dfs(node.right, targetSum)
 
-        return count + r1+r2+r3+r4       
+    def test(self, node: Optional[TreeNode], target: int):
+        if not node:
+            return
+        if node.val==target:
+            self.result+=1
+        target-=node.val
+        self.test(node.left,  target)
+        self.test(node.right, target)
+
+
 
 x = Solution()
 arr = [1,None,2,None,3,None,4,None,5]
+num= 3
 
+print(x.pathSum(arr,num))
