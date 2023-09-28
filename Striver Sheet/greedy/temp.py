9fb9014bf18f1ac9df081dc146c98ad38a4bdbcb
Naman Sharma <namansharma18899@gmail.com>
2023-09-28 10:30:40 +0530

    Mod Greddy

--- Striver Sheet/greedy/temp.py
+++ Striver Sheet/greedy/temp.py
@@ -7,10 +7,10 @@ class Train:
 arr = [900, 940, 950, 1100, 1500, 1800]
 dep = [910, 1200, 1120, 1130, 1900, 2000]
 
-obj= list()
-for index in range(0,3):
-    obj.append(Train(arr[index],dep[index]))
+obj = list()
+for index in range(0, 3):
+    obj.append(Train(arr[index], dep[index]))
 
-obj.sort(key = lambda x: x.dep)
+obj.sort(key=lambda x: x.dep)
 for each in obj:
     print(each.arr)
--- Striver Sheet/greedy/train sequencing.py
+++ Striver Sheet/greedy/train sequencing.py
@@ -1,49 +1,50 @@
 #User function Template for python3
 from Leetcode.utils.utility import timer
+
+
 class Train:
     def __init__(self, arr, dep):
         self.arr = int(arr)
         self.dep = int(dep)
 
-class Solution:    
-    #Function to find the minimum number of platforms required at the
-    #railway station such that no train waits.
+
+class Solution:
+    """Function to find the minimum number of platforms required \
+    at the railway station such that no train waits.
+    """
     @timer
-    def minimumPlatform(self,arr,dep):
+    def minimumPlatform(self, arr, dep):
         n = len(arr)
         objs = list()
-        for index in range(0,n):
+        for index in range(0, n):
             objs.append(Train(str(arr[index]), str(dep[index])))
-        objs.sort(key = lambda x: x.arr)
-        count = 1
+        objs.sort(key=lambda x: x.arr)
         queue = [objs.pop(0).dep]
         index = 0
         for train in objs:
             found = False
-            ln=len(queue)
-            index=ln-1 if ln>0 else 0
-            while(index>=0):
-                if train.arr>=queue[index]:
-                    found=True
+            ln = len(queue)
+            index = ln-1 if ln > 0 else 0
+            while (index >= 0):
+                if train.arr >= queue[index]:
+                    found = True
                     queue[index] = train.dep
                     break
-                index-=1
+                index = index - 1
             if not found:
                 queue.append(train.dep)
-        # for each in queue:
-        #     print(each.arr,' ',each.dep)
         return len(queue)
-        
+
 
 def fun():
-    sol= Solution()
+    sol = Solution()
     arr = [int(x) for x in input().split(' ')]
     dep = [int(x) for x in input().split(' ')]
     return sol.minimumPlatform(arr, dep)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     tcs = int(input())
-    while(tcs>0):
-        print('res -> ',fun())#input().split(',')))
-        tcs-=1
+    while (tcs > 0):
+        print('res -> ', fun())
+        tcs -= 1
