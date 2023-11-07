063826d9c4ede53c71fd3857e8f97e82cf658666
Naman Sharma <namansharma18899@gmail.com>
2023-11-07 19:27:48 +0530

    Solved: nqueens

--- Striver Sheet/backtracing/nqueens.py
+++ Striver Sheet/backtracing/nqueens.py
@@ -9,42 +9,56 @@ class Solution:
         result = []
 
         def update_affected(row, col, affect=True):
-            for r_tmp in range(0, n):
-                arr[r_tmp][col] = 2 if affect else 0
-            for col_tmp in range(0, n):
-                arr[row][col_tmp] = 2 if affect else 0
+            for r_tmp in range(0, n): # Entire Column
+                arr[r_tmp][col] = arr[r_tmp][col]+2 if affect else arr[r_tmp][col]-2
+            for col_tmp in range(0, n): # Entire ROW
+                arr[row][col_tmp] = arr[row][col_tmp]+2 if affect else arr[row][col_tmp]-2
             row_tmp = row-1
             for col_tmp in range(col+1, n): #top right
-               arr[row_tmp][col_tmp] = 2 
+               if row_tmp not in range(0,n):
+                   break
+               arr[row_tmp][col_tmp] = arr[row_tmp][col_tmp]+2 if affect else arr[row_tmp][col_tmp]-2 
                row_tmp-=1
             row_tmp= row+1
             for col_tmp in range(col+1, n): #bottm right
-               arr[row_tmp][col_tmp] = 2 
-               row_tmp+=1
+                if row_tmp not in range(0,n):
+                   break
+                arr[row_tmp][col_tmp] = arr[row_tmp][col_tmp]+2 if affect else arr[row_tmp][col_tmp]-2 
+                row_tmp+=1
             row_tmp= row-1
             for col_tmp in range(col-1, -1, -1): #bottm right
-               arr[row_tmp][col_tmp] = 2 
+               if row_tmp not in range(0,n):
+                   break
+               arr[row_tmp][col_tmp] = arr[row_tmp][col_tmp]+2 if affect else arr[row_tmp][col_tmp]-2
                row_tmp-=1
             row_tmp= row+1
             for col_tmp in range(col-1, -1, -1): #bottm right
-               arr[row_tmp][col_tmp] = 2 
+               if row_tmp not in range(0,n):
+                   break
+               arr[row_tmp][col_tmp] = arr[row_tmp][col_tmp]+2 if affect else arr[row_tmp][col_tmp]-2 
                row_tmp+=1
  
         def solve(i, j, count):
             if count == n:
-                tmp = deepcopy(arr)
-                result.append(tmp)
+                res = []
+                for i_tmp in range(n):
+                    st = ""
+                    for j_tmp in range(n):
+                        st+= 'Q' if arr[i_tmp][j_tmp]==1 else '.' 
+                    res.append(st)
+                result.append(res)
                 return
             if i >= n or j >= n:
                 return
             for row in range(i, n):
                 for col in range(j, n):
-                    if arr[row][col] == 2:  # affected
+                    if arr[row][col]!=0:  # affected
                         continue
                     update_affected(row, col, True)
                     arr[row][col] = 1  # make it a queen
-                    solve(i + 1, 0, count + 1)
+                    solve(row + 1, 0, count + 1)
                     update_affected(row, col, False)
+                    arr[row][col] = 0
 
         solve(0, 0, 0)
         return result
