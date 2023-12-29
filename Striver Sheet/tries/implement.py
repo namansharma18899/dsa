fabf66ff5fbcc676e5ef8b16787aba88514911f9
Naman Sharma <namansharma18899@gmail.com>
2023-12-29 17:50:37 +0530

    added trie

--- Striver Sheet/tries/implement.py
+++ Striver Sheet/tries/implement.py
@@ -3,6 +3,8 @@ implementing tries:
 
 ( foo, bar, barz, baz )
 """
+from collections import defaultdict
+import os
 
 class Trie:
     def __init__(self):
@@ -43,3 +45,49 @@ class Trie:
 # obj.insert(word)
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
+
+class TrieNode:
+    def __init__(self, text=''):
+        self.children = {}
+        self.is_eow = False
+
+
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+
+    def insert(self, word):
+        current = self.root
+        for i, char in enumerate(word):
+            if char not in current.children:
+                prefix = word[0:i+1]
+                current.children[char] = TrieNode(prefix)
+            current = current.children[char]
+        current.is_eow = True
+
+    def find(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_eow
+    
+    def start_with(self, word):
+            node = self.root
+            for char in word:
+                if char not in node.children:
+                    return False
+                node = node.children[char]
+            return True
+
+
+if __name__ == "__main__":
+    obj = PrefixTree()
+    obj.insert("apple")
+    obj.insert('apraisel')
+    obj.insert('absolute')
+    obj.insert('blink')
+    obj.insert('bloke')
+    print(obj.start_with(word='app'))
