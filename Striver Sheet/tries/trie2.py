f59cad8c438d8619d5f4382e032440ffdbd5bb18
Naman Sharma <namansharma18899@gmail.com>
2024-01-03 22:18:10 +0530

    mod: trie2

--- Striver Sheet/tries/trie2.py
+++ Striver Sheet/tries/trie2.py
@@ -1,7 +1,7 @@
 class TrieNode:
-    def __init__(self, text=''):
+    def __init__(self, text=""):
         self.children = {}
-        self.count= 1
+        self.count = 1
         self.is_eow = False
 
 
@@ -9,18 +9,17 @@ class PrefixTree:
     def __init__(self):
         self.root = TrieNode()
 
-
     def insert(self, word):
         current = self.root
         for i, char in enumerate(word):
             if char not in current.children:
-                prefix = word[0:i+1]
+                prefix = word[0 : i + 1]
                 current.children[char] = TrieNode(prefix)
             else:
-                current.children[char].count+=1
+                current.children[char].count += 1
             current = current.children[char]
         current.is_eow = True
-    
+
     def frequency(self, word: str):
         current = self.root
         for char in word:
@@ -28,16 +27,15 @@ class PrefixTree:
                 return 0
             current = current.children[char]
         return current.count
-    
+
     def remove(self, word):
         current = self.root
         for char in word:
             if char not in current.children:
                 return 0
-            current.children[char].count-=1
+            current.children[char].count -= 1
             current = current.children[char]
         return current.count
- 
 
     def find(self, word):
         node = self.root
@@ -46,20 +44,20 @@ class PrefixTree:
                 return False
             node = node.children[char]
         return node.is_eow
-    
-    def start_with(self, word):
-            node = self.root
-            for char in word:
-                if char not in node.children:
-                    return False
-                node = node.children[char]
-            return True
 
-    
+    def start_with(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
 x = PrefixTree()
 x.insert("samsung")
 x.insert("samsung")
 x.insert("vivo")
 x.remove("vivo")
-print(x.frequency('samsung'))
-print(x.frequency('vi'))
+print(x.frequency("samsung"))
+print(x.frequency("vi"))
