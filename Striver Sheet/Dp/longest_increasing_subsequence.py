e19f39ab1e63fb479ae07358c54837191f1f4a86
Naman Sharma <namansharma18899@gmail.com>
2024-07-18 22:12:40 +0530

    gave a try, feeling sleepy...couldn't finish approach, will try after waking up

--- Striver Sheet/Dp/longest_increasing_subsequence.py
+++ Striver Sheet/Dp/longest_increasing_subsequence.py
@@ -1,13 +1,29 @@
-arr = [10,9,2,5,3,7,101,18]
+arr = [10, 9, 2, 5, 3, 7, 101, 18]
 from typing import List
-
 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        self.mem_dp_arr = [None for _ in range(len(nums))]
-        def find_lis(nums, index):
-            if not self.mem_dp_arr[index]:
-                pass
-        return 0
+        self.mem = [1 for _ in range(len(nums))]
 
+        def calc_lis(nums, start_index, p_num, index, p_sum):
+            if index == len(nums) - 1:
+                self.mem[start_index] = max(p_sum, self.mem[start_index])
+                return p_sum
+                # BASE COND.FOR RECUR
+            # Consider this element if increasing or move forward
+            if not self.mem[index]:
+                pass
+            else:
+                pass
+            calc_lis(nums, start_index, nums[index], index+1, p_sum+1 if nums[index]>p_num else p_sum)
+            calc_lis(nums, start_index, p_num, index+1, p_sum)
+
+            # if self.mem[index]:
+            # if nums[index] > p_num:
+            #     self.mem[start_index] = max(self.mem[start_index], p_sum + 1)
+            # else:
+            #     self.mem[start_index]
+                # return self.mem[index]
+        calc_lis(nums, 1, nums[0], 1, 1)
+        return max(self.mem)
