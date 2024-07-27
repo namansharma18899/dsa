9c52382dca1ac1093e179d3bed7be8dcab604d3e
Naman Sharma <namansharma18899@gmail.com>
2024-07-27 14:40:42 +0530

    added lis first try, quite rusty

--- Striver Sheet/Dp/longest_increasing_subsequence.py
+++ Striver Sheet/Dp/longest_increasing_subsequence.py
@@ -1,29 +1,45 @@
-arr = [10, 9, 2, 5, 3, 7, 101, 18]
 from typing import List
 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        self.mem = [1 for _ in range(len(nums))]
+        self.mem = [0 for _ in range(len(nums))]
+        def calc_lis(nums,ind, prev_ind):
+            def is_curr_part_of_lis(nums,ind, prev_ind) -> bool: 
+                return nums[ind]>nums[prev_ind]
+            if ind>=len(nums):
+                return 0
+            chosen_curr = 0
+            skipped_curr = 0
+            if ind==prev_ind or is_curr_part_of_lis(nums,ind, prev_ind):
+                if self.mem[ind]==0:
+                    self.mem[ind] = 1 + calc_lis(nums, ind+1, ind+0)
+                chosen_curr = self.mem[ind]
+            skipped_curr = calc_lis(nums, ind+1, prev_ind)
+            return max(skipped_curr, chosen_curr)
+        for each in range(0, len(nums)):
+            calc_lis(nums, each, each+0)
+        print(self.mem)
+        return max(max(self.mem),1)
 
-        def calc_lis(nums, start_index, p_num, index, p_sum):
-            if index == len(nums) - 1:
-                self.mem[start_index] = max(p_sum, self.mem[start_index])
-                return p_sum
-                # BASE COND.FOR RECUR
-            # Consider this element if increasing or move forward
-            if not self.mem[index]:
-                pass
-            else:
-                pass
-            calc_lis(nums, start_index, nums[index], index+1, p_sum+1 if nums[index]>p_num else p_sum)
-            calc_lis(nums, start_index, p_num, index+1, p_sum)
 
-            # if self.mem[index]:
-            # if nums[index] > p_num:
-            #     self.mem[start_index] = max(self.mem[start_index], p_sum + 1)
-            # else:
-            #     self.mem[start_index]
-                # return self.mem[index]
-        calc_lis(nums, 1, nums[0], 1, 1)
-        return max(self.mem)
+from typing import List
+
+def fun(args):
+    sol= Solution()
+    return sol.lengthOfLIS(args)
+
+
+if __name__=='__main__':
+    # tcs = int(input())
+    tcs=[
+        [10, 9, 2, 5, 3, 7, 101, 18],
+        [0,1,0,3,2,3],
+        [1,1,1,1,1,1]
+    ]
+    for each in tcs:
+        print('testcase -> \n')
+        print(fun(each))
+    # while(tcs>0):
+    #     print('res -> ',fun())
+    #     tcs-=1
