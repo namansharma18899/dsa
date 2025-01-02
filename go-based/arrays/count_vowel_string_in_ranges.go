23feac1298ad506a8c6b96661e00bc13e7ea948b
Naman Sharma <namansharma18899@gmail.com>
2025-01-02 23:49:35 +0530

    Updated version, more polished

--- go-based/arrays/count_vowel_string_in_ranges.go
+++ go-based/arrays/count_vowel_string_in_ranges.go
@@ -1,38 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func isWordVowel(word string) bool {
-	vowels := "aeiouAEIOU"
-	if strings.ContainsRune(vowels, rune(word[0])) {
-		return strings.ContainsRune(vowels, rune(word[len(word)-1]))
-	} else {
-		return false
-	}
+func isVowel(c byte) bool {
+	return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
 }
+
 func vowelStrings(words []string, queries [][]int) []int {
-	var result []int
-	var prefixSum []int
-	var isVowelArr []int
-	// vowelBoolArr := make([]int, 0)
-	for _, eachWord := range words {
-		// prefix[i+1] = prefix[i] + words[i]
-		if isWordVowel(eachWord) {
-			isVowelArr = append(isVowelArr, 1)
-		} else {
-			isVowelArr = append(isVowelArr, 0)
+	n := len(words)
+	prefix := make([]int, n)
+
+	for idx, word := range words {
+		prefix[idx] = 0
+		if idx > 0 {
+			prefix[idx] = prefix[idx-1]
+		}
+		// Here we are finding Prefix Sum...while finding vowels
+		if isVowel(word[0]) && isVowel(word[len(word)-1]) {
+			prefix[idx]++
 		}
 	}
-	prefixSum = append(prefixSum, isVowelArr[0])
-	for i := 1; i < len(isVowelArr); i++ {
-		prefixSum = append(prefixSum, (prefixSum[i-1] + isVowelArr[i]))
+	sols := make([]int, len(queries))
+	for idx, query := range queries {
+		l := query[0]
+		r := query[1]
+		if l == 0 {
+			sols[idx] = prefix[r]
+			continue
+		}
+		// Core logix
+		sols[idx] = prefix[r] - prefix[l-1]
 	}
-	fmt.Println(isVowelArr)
-	for i := range queries {
-		result = append(result, prefixSum[queries[i][1]]-prefixSum[queries[i][0]]+isVowelArr[queries[i][0]])
-	}
-	return result
+	return sols
 }
