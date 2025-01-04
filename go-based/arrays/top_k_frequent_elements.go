9547a66dd5f4524920994dfded8dff83206ec34f
Naman Sharma <namansharma18899@gmail.com>
2025-01-04 22:36:40 +0530

    1ms response time on LC, GODAMMM

--- go-based/arrays/top_k_frequent_elements.go
+++ go-based/arrays/top_k_frequent_elements.go
@@ -9,6 +9,8 @@ func topKFrequent(nums []int, k int) []int {
 	frequencyMap := make(map[int][]int)
 	prev := nums[0]
 	counter := 1
+	result := make([]int, 0)
+
 	for index := 1; index < len(nums); index++ {
 		if nums[index] == prev {
 			counter++
@@ -20,9 +22,19 @@ func topKFrequent(nums []int, k int) []int {
 	}
 	// for the last element
 	frequencyMap[counter] = append(frequencyMap[counter], prev)
-	for index := len(nums) - 1; index > 0; index-- {
+	for index := len(nums); index >= 0; index-- {
+		if k <= 0 {
+			break
+		}
 		if val, isThere := frequencyMap[index]; isThere {
-
+			for j := 0; j < len(val); j++ {
+				if k <= 0 {
+					break
+				}
+				result = append(result, val[j])
+				k--
+			}
 		}
 	}
+	return result
 }
