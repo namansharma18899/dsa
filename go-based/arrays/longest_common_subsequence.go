f286356676f3f50c9ee5be4a27551760a63f8794
Naman Sharma <namansharma18899@gmail.com>
2025-01-07 16:55:04 +0530

    minor edge cases checkins

--- go-based/arrays/longest_common_subsequence.go
+++ go-based/arrays/longest_common_subsequence.go
@@ -3,20 +3,24 @@ package main
 import "fmt"
 
 func longestConsecutive(nums []int) int {
-	if len(nums) == 1 {
-		return 1
+	// Logic, if a number before me exists, then the subsequence is valid
+	if len(nums) <= 1 {
+		return len(nums)
 	}
 	maxVal := nums[0]
+	minVal := nums[0]
 	mapOFElements := make(map[int]int)
 	for index := 0; index < len(nums); index++ {
 		mapOFElements[nums[index]] = 1
 		if nums[index] > maxVal {
 			maxVal = nums[index]
 		}
+		if nums[index] < minVal {
+			minVal = nums[index]
+		}
 	}
 	result := 0
-	fmt.Println("mapxxx -> ", mapOFElements)
-	for index := 0; index < maxVal+1; index++ {
+	for index := minVal; index < maxVal+1; index++ {
 		if mapOFElements[index] > 0 {
 			if mapOFElements[index-1] > 0 {
 				mapOFElements[index] += mapOFElements[index-1]
