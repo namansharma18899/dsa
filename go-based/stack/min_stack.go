2bf7b94d90f393f157991a2a9dfa2545c0e987c4
Naman Sharma <namansharma18899@gmail.com>
2025-01-20 20:28:08 +0530

    stack

---
+++ go-based/stack/min_stack.go
@@ -0,0 +1,33 @@
+package main
+
+type MinStack struct {
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (this *MinStack) Push(val int) {
+
+}
+
+func (this *MinStack) Pop() {
+
+}
+
+func (this *MinStack) Top() int {
+	return 0
+}
+
+func (this *MinStack) GetMin() int {
+	return 0
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(val);
+ * obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.GetMin();
+ */
