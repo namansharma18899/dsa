baedd9db8a9fc695a4a112795c855da2f08ffe76
Naman Sharma <namansharma18899@gmail.com>
2025-01-20 20:28:59 +0530

    stack impl

--- go-based/stack/min_stack.go
+++ go-based/stack/min_stack.go
@@ -1,26 +1,51 @@
 package main
 
+import "math"
+
+type Node struct {
+	Val  int
+	Prev *Node
+	Min  int
+}
+
 type MinStack struct {
+	head *Node
 }
 
 func Constructor() MinStack {
-	return MinStack{}
+	return MinStack{
+		head: &Node{
+			Min: math.MaxInt32,
+		},
+	}
 }
 
 func (this *MinStack) Push(val int) {
-
+	min := Min(this.head.Min, val)
+	this.head = &Node{
+		Val:  val,
+		Prev: this.head,
+		Min:  min,
+	}
 }
 
 func (this *MinStack) Pop() {
-
+	this.head = this.head.Prev
 }
 
 func (this *MinStack) Top() int {
-	return 0
+	return this.head.Val
 }
 
 func (this *MinStack) GetMin() int {
-	return 0
+	return this.head.Min
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 /**
